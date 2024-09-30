d39c4664f8c5dba5c6f214c174f55b60a02a2528
marcioGabrielMengali <marciogabrielmengali@gmail.com>
2024-09-30 19:04:36 -0300

    feat: adding const for node_env

--- src/shared/consts/const.ts
+++ src/shared/consts/const.ts
@@ -1,8 +1,10 @@
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-export const PORT = process.env.PORT || 3000
-export const URL = `http://localhost:${PORT}`
-export const URL_PRODUCTS = 'product'
-export const LOG_FILE = process.env.LOG_FILE || 'logs/error.log'
-export const COMBINED_LOG_FILE = process.env.COMBINED_LOG_FILE || 'logs/combined.log'
+export const PORT = process.env.PORT || 3000;
+export const URL = `http://localhost:${PORT}`;
+export const URL_PRODUCTS = "product";
+export const LOG_FILE = process.env.LOG_FILE || "logs/error.log";
+export const COMBINED_LOG_FILE =
+  process.env.COMBINED_LOG_FILE || "logs/combined.log";
+export const NODE_ENV = process.env.NODE_ENV || "dev";
--- src/shared/log/logger.ts
+++ src/shared/log/logger.ts
@@ -1,34 +1,29 @@
-import { createLogger, format, transports } from 'winston';
-import { Format } from 'logform';
-import { COMBINED_LOG_FILE, LOG_FILE } from '../consts/const';
-
-
+import { createLogger, format, transports } from "winston";
+import { Format } from "logform";
+import { COMBINED_LOG_FILE, LOG_FILE, NODE_ENV } from "../consts/const";
 
 const logFormat: Format = format.printf(({ timestamp, level, message }) => {
-    return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+  return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
 });
 
 const logger = createLogger({
-    level: 'info',
-    format: format.combine(
-        format.timestamp(),
-        format.errors({ stack: true }),
-        format.splat(),
-        logFormat
-    ),
-    transports: [
-        new transports.Console({
-            format: format.combine(
-                format.colorize(),
-                logFormat
-            ),
-        }),
-        new transports.File({
-            filename: LOG_FILE,
-            level: 'error',
-        }),
-        new transports.File({ filename: COMBINED_LOG_FILE }),
-    ],
+  level: NODE_ENV,
+  format: format.combine(
+    format.timestamp(),
+    format.errors({ stack: true }),
+    format.splat(),
+    logFormat
+  ),
+  transports: [
+    new transports.Console({
+      format: format.combine(format.colorize(), logFormat),
+    }),
+    new transports.File({
+      filename: LOG_FILE,
+      level: "error",
+    }),
+    new transports.File({ filename: COMBINED_LOG_FILE }),
+  ],
 });
 
 export default logger;
