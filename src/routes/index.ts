c18a976318bbf624fb0ec2402c7dee8cef6c8bbb
marcioGabrielMengali <marciogabrielmengali@gmail.com>
2024-10-01 15:22:15 -0300

    fix: url products

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { productRoutes } from "./productRoutes";
+import { URL_PRODUCTS } from "../shared/consts/const";
 
 const routes = Router();
 
-routes.use("/product", productRoutes);
+routes.use(`/${URL_PRODUCTS}`, productRoutes);
 
 export default routes;
